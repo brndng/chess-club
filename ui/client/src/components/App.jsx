d72bc2b0ffc00cb71e262e1cfe7773488310d0a6
Brian Duong <brndng89@gmail.com>
2018-04-24 11:09:36 -0700

    [matrix] with coordinates

--- ui/client/src/components/App.jsx
+++ ui/client/src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Board from './Board.jsx';
 
 const App = () => {
-  return <div>Hello from App!</div>;
+
+  const initBoard = () => {
+    const board = [];
+    const files = 'hgfedcba';
+    for (let i=0; i<8; i++) {
+      let row = [];
+      for (let j=0; j<8; j++) {
+        let rank = i+1;
+        let file = files[j];
+        row[j] = file + rank;
+      }
+      board[i] = row;
+    }
+    console.log(board);
+    return board;
+  }
+
+  // const rotateBoard = (matrix) => {
+  // }
+  
+  return <div>Hello from App!!!{initBoard()}</div>;
 }
 
 export default App;
