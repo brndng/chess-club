9a14968e50979ef418e38e136df49f31b69ba60d
Brian Duong <brndng89@gmail.com>
2018-06-25 14:06:01 -0700

    [server] combining servers. 1. match static files, 2. match api requests 3. wildcard

--- api/rest-server/index.js
+++ api/rest-server/index.js
@@ -1,39 +1,18 @@
 const express = require('express');
 const path = require('path');
-const parser = require('body-parser');
-const session = require('express-session');
-const cors = require('cors');
-const helmet = require('helmet');
 const sequelize = require('../db');
 const router = require('./routes');
+const { apiMiddleware } = require('./middleware/');
 
 const app = express();
 const PORT = 3000;
 
-const middleware = [
-  helmet(),
-  parser.json(),
-  parser.urlencoded({ extended: true }),
-  session({
-    secret: 'secret',
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-      httpOnly: false,
-      secure: false,
-    },
-  }),
-  cors({
-    credentials: true,
-    origin: 'http://localhost:8080',
-    allowedHeaders: 'Content-Type, authorization',
-    methods: ['GET, POST, PUT, DELETE', 'OPTIONS'],
-  }),
-];
-
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
-app.use(...middleware);
+
+app.use(express.static(path.join(__dirname, '../../ui/client/dist/')));
+app.use(...apiMiddleware);
 app.use(router);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../ui/client/dist/index.html')));
 
 sequelize.sync().then(() => console.log('DB synced'));
 
