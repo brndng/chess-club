1d7f4ff20e5e8285d94efe60d9b0672801266076
Brian Duong <brndng89@gmail.com>
2018-06-26 15:01:59 -0700

    [dotenv]

--- api/server/index.js
+++ api/server/index.js
@@ -5,9 +5,10 @@ const sequelize = require('../db');
 const router = require('./routes');
 const createSocketHandlers = require('./socket/');
 const { apiMiddleware } = require('./middleware/');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
 const io = socket(server);
 
@@ -23,7 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../ui/client/dist/')));
   app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../ui/client/dist/index.html')));
 }
-
 
 sequelize.sync().then(() => console.log('DB synced'));
 
