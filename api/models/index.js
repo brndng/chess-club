e6d08cdca17bfcb1c6b47987f1a4394f90b9a95c
Brian Duong <brndng89@gmail.com>
2018-06-28 10:51:08 -0700

    [deploy] please

--- api/models/index.js
+++ api/models/index.js
@@ -7,8 +7,8 @@ if (!global.hasOwnProperty('db')) {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
+      // port:     match[4],
+      // host:     match[3],
       logging:  false
     });
   } else {
