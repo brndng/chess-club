ddb7ebe397041cb4d27a6486e7fe6e4ae936b3c9
Brian Duong <brndng89@gmail.com>
2018-06-28 11:43:04 -0700

    [deploy] please

--- api/models/index.js
+++ api/models/index.js
@@ -5,11 +5,11 @@ if (!global.hasOwnProperty('db')) {
   if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
+      // dialect:  'postgres',
+      // protocol: 'postgres',
       // port:     match[4],
       // host:     match[3],
-      logging:  false
+      logging:  true
     });
   } else {
     // local
