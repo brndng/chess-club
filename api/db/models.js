7c9a52477e3c226e6c21d0ab3827e2d598461277
Brian Duong <brndng89@gmail.com>
2018-06-28 09:16:12 -0700

    [deploy] test

--- api/db/models.js
+++ api/db/models.js
@@ -1,30 +1,32 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./index.js');
 
-const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  wins: Sequelize.INTEGER,
-  losses: Sequelize.INTEGER,
-  draws: Sequelize.INTEGER,
-});
 
-const Game = sequelize.define('game', {
-  position: Sequelize.ARRAY(Sequelize.TEXT),
-  moves: Sequelize.JSON,
-  whiteToMove: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
-  inCheck: Sequelize.INTEGER,
-  whiteUsername: Sequelize.STRING,
-  blackUsername: Sequelize.STRING,
-  drawOfferedBy: Sequelize.INTEGER,
-  positionHistory: { type: Sequelize.JSON, allowNull: false, defaultValue: [] },
-  completed: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
-  winner: Sequelize.INTEGER,
-  result: Sequelize.STRING,
-});
+// // Models
+// const User = sequelize.define('user', {
+//   username: Sequelize.STRING,
+//   password: Sequelize.STRING,
+//   wins: Sequelize.INTEGER,
+//   losses: Sequelize.INTEGER,
+//   draws: Sequelize.INTEGER,
+// });
 
-// Associations
-Game.belongsTo(User, { foreignKey: 'white' });
-Game.belongsTo(User, { foreignKey: 'black' });
+// const Game = sequelize.define('game', {
+//   position: Sequelize.ARRAY(Sequelize.TEXT),
+//   moves: Sequelize.JSON,
+//   whiteToMove: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
+//   inCheck: Sequelize.INTEGER,
+//   whiteUsername: Sequelize.STRING,
+//   blackUsername: Sequelize.STRING,
+//   drawOfferedBy: Sequelize.INTEGER,
+//   positionHistory: { type: Sequelize.JSON, allowNull: false, defaultValue: [] },
+//   completed: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
+//   winner: Sequelize.INTEGER,
+//   result: Sequelize.STRING,
+// });
 
-module.exports = { User, Game };
+// // Associations
+// Game.belongsTo(User, { foreignKey: 'white' });
+// Game.belongsTo(User, { foreignKey: 'black' });
+
+// module.exports = { User, Game };
